fb8f3f9f8c52b36e16b7d89b1e3b76b19da5ae84
albir <albir@buubuu.com>
2023-06-17 22:05:03 +0700

    project albi

---
+++ src/Contact.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+class Contact extends Component {
+ render() {
+    return (
+        <div>
+            <h2>Contact</h2>
+            <p>Email: albirannisa52@gmail.com <br />
+             Phone: 081376531890
+             WA : 081376531890
+</p>
+        </div>
+        );
+    }
+}
+
+export default Contact;
